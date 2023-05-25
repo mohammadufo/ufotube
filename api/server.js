d9208b6d8250d2bd60c1240f308229dc7ce71fb3
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-05-25 18:05:34 +0330

    add base routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoute from './routes/users.js'
+import videoRoute from './routes/videos.js'
+import commentRoute from './routes/comments.js'
+import authRoute from './routes/auth.js'
 
 const app = express()
 dotenv.config()
@@ -11,6 +15,11 @@ const connect = () => {
     .then(() => console.log('Connected to Database'))
     .catch((err) => console.log('oh there is an error 💥', err))
 }
+
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/videos', videoRoute)
+app.use('/api/comments', commentRoute)
 
 const PORT = 8800
 app.listen(PORT, () => {
