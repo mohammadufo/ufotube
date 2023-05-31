47ff4d8901d0f4a980493e3fce61977349db9bf9
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-06-01 01:13:47 +0330

    add cors

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
 const connect = () => {
   mongoose
